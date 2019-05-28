f0257c85ed80bcd0637316f4704a3551dda55746
shariff6 <shariff.mohammed.a@gmail.com>
2019-05-28 14:35:36 +0300

    adds route to specific stylist page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -41,6 +41,13 @@ public class App {
           model.put("template", "templates/index.vtl");
           return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+        get("/stylists/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+            model.put("stylist", stylist);
+            model.put("template", "templates/stylist.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
 
       }
 }
