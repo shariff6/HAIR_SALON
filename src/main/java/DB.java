2783de18ca66d824fbd5750436c3ea3f1cd68228
shariff6 <shariff.mohammed.a@gmail.com>
2019-06-03 11:09:49 +0300

    heroku db configuration

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,7 @@
 import org.sql2o.*;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", null, null);
+  String connectionString = "jdbc:  postgres://mjgdptdetrrkcp:c373c5980ac84aa497cbe6ab43f3ca2d2f6503e9f366f528db9f61de7c07b39d@ec2-107-20-155-148.compute-1.amazonaws.com:5432/db32urhq9pdd4d"; //!
+    Sql2o sql2o = new Sql2o(connectionString, "mjgdptdetrrkcp", "c373c5980ac84aa497cbe6ab43f3ca2d2f6503e9f366f528db9f61de7c07b39d"); //!
+
 }
